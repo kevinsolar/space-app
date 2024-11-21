f6d4c75d57254e7f20fadc0579d5e70dd9f3aeba
Kevin BTA <kevinleitesolar@hotmail.com>
2024-11-21 11:43:47 -0300

    - Criando nosso componente que será responsáel pelo reset do css; - Estilizando nosso componente de Backgorund do app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import styled from "styled-components"
+import EstiloGlobais from "./componentes/EstilosGlobais"
 
+const FundoGradiente = styled.div`
+   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+   width: 100%;
+   min-height: 100vh;
+`
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <FundoGradiente>
+      <EstiloGlobais />
+    </FundoGradiente>
   )
 }
 
